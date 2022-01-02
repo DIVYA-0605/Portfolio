7efe5b340da4d19ad635ebf14b01aee70c118811
DIVYA525-VAD <68990221+DIVYA525-VAD@users.noreply.github.com>
2022-01-02 19:23:03 +0530

    portfolio

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,8 +22,8 @@ const Footer = ({ data }) => {
           <ul className="copyright">
             <li>
               Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
-                PAPA
+              <a title="DIVYA" href="https://github.com/DIVYA-0605/">
+                DIVYA
               </a>
             </li>
           </ul>
